866391c9bf52c7d8cbee1f23e5a18377e5560f57
bbompk <panithiv@gmail.com>
2024-02-08 18:49:26 +0700

    fix: cors

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -21,11 +22,11 @@ func main() {
 		c.JSON(200, gin.H{"message": "ok"})
 	})
 
-	// Init Router
-	apis.InitRouter(r)
-
 	// CORS
 	r.Use(CORSMiddleware())
+
+	// Init Router
+	apis.InitRouter(r)
 
 	// Start Server
 	httpPort := os.Getenv("HTTP_PORT")
@@ -40,9 +41,8 @@ func main() {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		log.Println("Handling CORS :D")
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", fmt.Sprintf("้http://localhost:5173, http://localhost:3000, %s", os.Getenv("FRONTEND_URL")))
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
