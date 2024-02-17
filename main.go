ead76c808601d77dfad02aef36d15a81399f3d4d
teeradon21 <teeradon21@outlook.com>
2024-02-17 13:58:39 +0700

    add swagger ui page

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/protengplus/proteng-bff/apis"
@@ -11,7 +12,34 @@ import (
 	"github.com/gin-contrib/cors"
 	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
+
+	docs "github.com/protengplus/proteng-bff/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /helloworld [get]
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "helloworld")
+}
+
+// HealthCheck godoc
+// @Summary Health Check Endpoint
+// @Description Check the health of the service
+// @Produce json
+// @Success 200 {object} models.HttpResponseOK "Successful operation"
+// @Router /health [get]
+func HealthCheck(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "ok"})
+}
 
 func main() {
 	logger.InitZap()
@@ -20,10 +48,14 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
 
+	docs.SwaggerInfo.BasePath = "/"
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	r.GET("/helloworld", Helloworld)
+
 	// Health Check Endpoint
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "ok"})
-	})
+	r.GET("/health", HealthCheck)
 
 	// CORS
 	r.Use(CORSMiddleware())
