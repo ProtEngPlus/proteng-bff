1d48eff73f43a3d0d558412f283815e1f8c52b66
teeradon21 <teeradon21@outlook.com>
2024-02-18 11:05:41 +0700

    edit forward add params

--- apis/forwarder.go
+++ apis/forwarder.go
@@ -13,33 +13,13 @@ import (
 	"github.com/protengplus/proteng-bff/utils/apiutil"
 )
 
-// ForwardNoStrict is Forward but the request and response body can be anything
-func ForwardNoStrict(baseUrl string, params ...map[string]interface{}) func(c *gin.Context) {
-	return ForwardStrict[interface{}, interface{}](baseUrl, params...)
-}
-
 // Forward the request to another service with the common response format
-func Forward(baseUrl string, params ...map[string]interface{}) func(c *gin.Context) {
-	return ForwardStrict[models.HttpResponse, interface{}](baseUrl, params...)
+func Forward(baseUrl string) func(c *gin.Context) {
+	return ForwardStrict[models.HttpResponse, interface{}](baseUrl)
 }
 
-// ForwardStrict will have to specify the request and response struct
-func ForwardStrict[Resp any, Req any](baseUrl string, params ...map[string]interface{}) func(c *gin.Context) {
+func ForwardAddParam(baseUrl string, params map[string]interface{}) func(c *gin.Context) {
 	return func(gctx *gin.Context) {
-		var body Req
-		err := gctx.Bind(&body)
-		if err != nil {
-			apiutil.ApiResponseErrorBadRequest(gctx, err)
-			return
-		}
-
-		var bodyBuffer []byte
-		bodyBuffer, err = json.Marshal(body)
-		if err != nil {
-			apiutil.ApiResponseInternalServerError(gctx, err)
-			return
-		}
-
 		// Construct URL with query parameters
 		u, err := url.Parse(baseUrl)
 		if err != nil {
@@ -47,31 +27,48 @@ func ForwardStrict[Resp any, Req any](baseUrl string, params ...map[string]inter
 			return
 		}
 
-		if len(params) > 0 {
-			q := u.Query()
-			for key, value := range params[0] {
-				// Convert the value to string before setting it in the query
-				q.Set(key, fmt.Sprintf("%v", value))
-			}
-			u.RawQuery = q.Encode()
+		q := u.Query()
+		for key, value := range params {
+			q.Set(key, fmt.Sprintf("%v", value))
 		}
+		u.RawQuery = q.Encode()
 
-		fmt.Println(u)
+		// Call ForwardStrict with the modified URL
+		ForwardStrict[models.HttpResponse, interface{}](u.String())(gctx)
+	}
+}
 
-		req, err := http.NewRequestWithContext(gctx, gctx.Request.Method, u.String(), bytes.NewReader(bodyBuffer))
+// ForwardNoStrict is Forward but the request and response body can be anything
+func ForwardNoStrict(url string) func(c *gin.Context) {
+	return ForwardStrict[interface{}, interface{}](url)
+}
+
+// ForwardStrict will have to specify the request and response struct
+func ForwardStrict[Resp any, Req any](url string) func(c *gin.Context) {
+	return func(gctx *gin.Context) {
+		var body Req
+		err := gctx.Bind(&body)
+		if err != nil {
+			apiutil.ApiResponseErrorBadRequest(gctx, err)
+			return
+		}
+		var bodyBuffer []byte
+		bodyBuffer, err = json.Marshal(body)
 		if err != nil {
 			apiutil.ApiResponseInternalServerError(gctx, err)
 			return
 		}
-
+		req, err := http.NewRequestWithContext(gctx, gctx.Request.Method, url, bytes.NewReader(bodyBuffer))
+		if err != nil {
+			apiutil.ApiResponseInternalServerError(gctx, err)
+			return
+		}
 		client := http.Client{}
 		resp, err := client.Do(req)
 		if err != nil {
 			apiutil.ApiResponseInternalServerError(gctx, err)
 			return
 		}
-		defer resp.Body.Close()
-
 		var respBody Resp
 		err = json.NewDecoder(resp.Body).Decode(&respBody)
 		if err != nil {
@@ -82,7 +79,6 @@ func ForwardStrict[Resp any, Req any](baseUrl string, params ...map[string]inter
 			gctx.JSON(http.StatusInternalServerError, respBody)
 			return
 		}
-
 		gctx.JSON(http.StatusOK, respBody)
 	}
 }
--- apis/router.go
+++ apis/router.go
@@ -10,8 +10,8 @@ import (
 func InitRouter(r *gin.Engine) {
 
 	userRouter := r.Group("/proteng-user-mgmt")
-
 	usermgmtUrl := configs.Config.UserMgmtUrl
+
 	userRouter.GET("/users", middleware.Authenticate(), middleware.Authorize("admin"), Forward(usermgmtUrl+"/users"))
 	userRouter.GET("/users/:id", middleware.Authenticate(), middleware.Authorize("admin"), func(c *gin.Context) { Forward(usermgmtUrl + "/users/" + c.Param("id"))(c) })
 	userRouter.POST("/users", Forward(usermgmtUrl+"/users"))
@@ -30,13 +30,15 @@ func InitRouter(r *gin.Engine) {
 	userRouter.POST("/auth/login/admin", Forward(usermgmtUrl+"/auth/login/admin"))
 
 	conductorRouter := r.Group("/proteng-conductor")
-
 	conductorUrl := configs.Config.ConductorUrl
+
 	conductorRouter.GET("/jobs", middleware.Authenticate(), middleware.Authorize("user"), func(c *gin.Context) {
-		Forward(conductorUrl+"/jobs?"+c.Request.URL.RawQuery, map[string]interface{}{"user_id": c.GetString("userId")})(c)
+		ForwardAddParam(conductorUrl+"/jobs?"+c.Request.URL.RawQuery, map[string]interface{}{"user_id": c.GetString("userId")})(c)
 	})
 	conductorRouter.GET("/jobs/:id", middleware.Authenticate(), func(c *gin.Context) { Forward(conductorUrl + "/jobs/" + c.Param("id"))(c) })
-	conductorRouter.POST("/jobs", middleware.Authenticate(), middleware.Authorize("user"), Forward(conductorUrl+"/jobs"))
+	// conductorRouter.POST("/jobs", middleware.Authenticate(), middleware.Authorize("user"), func(c *gin.Context) {
+	// 	ForwardAddBody(conductorUrl+"/jobs", map[string]interface{}{"user_id": c.GetString("userId")})(c)
+	// })
 	conductorRouter.PUT("/jobs/:id", middleware.Authenticate(), middleware.Authorize("user", "staff"), func(c *gin.Context) { Forward(conductorUrl + "/jobs/" + c.Param("id"))(c) })
 	conductorRouter.DELETE("/jobs/:id", func(c *gin.Context) { Forward(conductorUrl + "/jobs/" + c.Param("id"))(c) })
 	conductorRouter.POST("/jobs/:id/run", middleware.Authenticate(), middleware.Authorize("user"), func(c *gin.Context) { Forward(conductorUrl + "/jobs/" + c.Param("id") + "/run")(c) })
